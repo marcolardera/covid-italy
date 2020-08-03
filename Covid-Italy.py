b002b9d1ae793703b38b2b63bb112018ae2095b6
marcolardera <43131575+marcolardera@users.noreply.github.com>
2020-08-03 21:28:28 +0200

    Switched to Plotly for performance reasons

--- Covid-Italy.py
+++ Covid-Italy.py
@@ -7,20 +7,8 @@ import datetime
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+import plotly.express as px
 from PIL import Image
-
-#Annotate a matplotlib/seaborn barplot with the value of each patch
-def annotate_barplot (plot):
-	for p in plot.patches:
-		plot.text (p.get_x()+p.get_width()/2, p.get_height(), int(p.get_height()),
-		ha="center", 
-		fontsize="xx-large",
-		fontweight="bold")
-
-plt.rcParams ["figure.figsize"]=[16,8]
-sns.set_style ("darkgrid")
 
 source_naz="https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv"
 source_reg="https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv"
@@ -42,9 +30,11 @@ df_naz.replace (np.nan, 0, inplace=True)
 df_reg.replace ([np.nan,np.inf], 0, inplace=True)
 df_reg_ranking.rename (columns={"totale_casi":"Casi"}, inplace=True)
 
-#Workaround for hiding the Streamlit menu
-hide_menu_style = "<style>#MainMenu {visibility: hidden;}</style>"
-st.markdown (hide_menu_style, unsafe_allow_html=True)
+#Workaround for hiding unwanted Streamlit elements
+hide_style="""<style>#MainMenu {visibility: hidden;}
+			  div.block-container:nth-child(2) > div:nth-child(1) > div:nth-child(12) > div:nth-child(1) > button:nth-child(1) {display: none;}
+			  </style>"""
+st.markdown (hide_style, unsafe_allow_html=True)
 
 #*** SIDEBAR START ***
 
@@ -141,184 +131,190 @@ if select != "Italia":
 st.header ("Casi totali")
 
 if select=="Italia":
-	sns.lineplot (x=df_naz.index, y="totale_casi", marker="o", data=df_naz)
-	#plt.xticks(df_naz.index)
-	plt.xticks (range(0, len(df_naz.index), 5))
-	plt.yticks (fontsize=20)
-	plt.title ("Andamento casi totali nazionali", fontsize=20)
-	plt.xlabel ("Giorni", fontsize=20)
-	plt.ylabel ("Casi", fontsize=20)
-	st.pyplot ()
+
+	figure=px.line (df_naz, x="data", y="totale_casi", title="Andamento casi totali nazionali",
+				labels={
+				"data": "Data",
+				"totale_casi": "Casi"
+				})
+	st.plotly_chart (figure, use_container_width=True)
+
 else:
-	sns.lineplot (x=df_naz.index, y="totale_casi", marker="o", data=df_reg[df_reg["denominazione_regione"]==select])
-	plt.xticks (range(0, len(df_naz.index), 5))
-	plt.yticks (fontsize=20)
-	plt.title (f"Andamento casi totali {select}", fontsize=20)
-	plt.xlabel ("Giorni", fontsize=20)
-	plt.ylabel ("Casi", fontsize=20)
-	st.pyplot ()
+
+	figure=px.line (df_reg[df_reg["denominazione_regione"]==select], x="data", y="totale_casi", title=f"Andamento casi totali {select}",
+				labels={
+				"data": "Data",
+				"totale_casi": "Casi"
+				})
+	st.plotly_chart (figure, use_container_width=True)
 
 st.header ("Tamponi effettuati")
 
 if select=="Italia":
-	sns.lineplot (x=df_naz.index, y="tamponi", marker="o", data=df_naz)
-	plt.xticks (range(0, len(df_naz.index), 5))
-	plt.yticks (fontsize=20)
-	plt.title ("Andamento nazionale tamponi effettuati", fontsize=20)
-	plt.xlabel ("Giorni", fontsize=20)
-	plt.ylabel ("Tamponi", fontsize=20)
-	st.pyplot ()
+
+	figure=px.line (df_naz, x="data", y="tamponi", title="Andamento nazionale tamponi effettuati",
+				labels={
+				"data": "Data",
+				"tamponi": "Tamponi"
+				})
+	st.plotly_chart (figure, use_container_width=True)
+
 else:
-	sns.lineplot (x=df_naz.index, y="tamponi", marker="o", data=df_reg[df_reg["denominazione_regione"]==select])
-	plt.xticks (range(0, len(df_naz.index), 5))
-	plt.yticks (fontsize=20)
-	plt.title (f"Andamento tamponi effettuati {select}", fontsize=20)
-	plt.xlabel ("Giorni", fontsize=20)
-	plt.ylabel ("Tamponi", fontsize=20)
-	st.pyplot ()
+
+	figure=px.line (df_reg[df_reg["denominazione_regione"]==select], x="data", y="tamponi", title=f"Andamento tamponi effettuati {select}",
+				labels={
+				"data": "Data",
+				"tamponi": "Tamponi"
+				})
+	st.plotly_chart (figure, use_container_width=True)
 
 st.header ("Casi per tamponi")
 
 if select=="Italia":
-	sns.lineplot (x=df_naz.index, y="casi_per_tamponi", marker="o", data=df_naz)
-	plt.xticks (range(0, len(df_naz.index), 5))
-	plt.yticks (fontsize=20)
-	plt.title ("Andamento nazionale casi per tamponi", fontsize=20)
-	plt.xlabel ("Giorni", fontsize=20)
-	plt.ylabel ("Casi per tamponi", fontsize=20)
-	st.pyplot ()
+
+	figure=px.line (df_naz, x="data", y="casi_per_tamponi", title="Andamento nazionale casi per tamponi",
+				labels={
+				"data": "Data",
+				"casi_per_tamponi": "Casi per tamponi"
+				})
+	st.plotly_chart (figure, use_container_width=True)
+
 else:
-	sns.lineplot (x=df_naz.index, y="casi_per_tamponi", marker="o", data=df_reg[df_reg["denominazione_regione"]==select])
-	plt.xticks (range(0, len(df_naz.index), 5))
-	plt.yticks (fontsize=20)
-	plt.title (f"Andamento casi per tamponi {select}", fontsize=20)
-	plt.xlabel ("Giorni", fontsize=20)
-	plt.ylabel ("Casi per tamponi", fontsize=20)
-	st.pyplot ()
+
+	figure=px.line (df_reg[df_reg["denominazione_regione"]==select], x="data", y="casi_per_tamponi", title=f"Andamento casi per tamponi {select}",
+				labels={
+				"data": "Data",
+				"casi_per_tamponi": "Casi per tamponi"
+				})
+	st.plotly_chart (figure, use_container_width=True)
 
 st.header ("Nuovi casi giornalieri")
 
 if select=="Italia":
-	sns.lineplot (x=df_naz.index, y="nuovi_positivi", marker="o", data=df_naz)
-	plt.xticks (range(0, len(df_naz.index), 5))
-	plt.yticks (fontsize=20)
-	plt.title ("Andamento nazionale nuovi casi giornalieri", fontsize=20)
-	plt.xlabel ("Giorni", fontsize=20)
-	plt.ylabel ("Nuovi casi", fontsize=20)
-	st.pyplot ()
+
+	figure=px.line (df_naz, x="data", y="nuovi_positivi", title="Andamento nazionale nuovi casi giornalieri",
+				labels={
+				"data": "Data",
+				"nuovi_positivi": "Nuovi casi"
+				})
+	st.plotly_chart (figure, use_container_width=True)
+
 else:
-	sns.lineplot (x=df_naz.index, y="nuovi_positivi", marker="o", data=df_reg[df_reg["denominazione_regione"]==select])
-	plt.xticks (range(0, len(df_naz.index), 5))
-	plt.yticks (fontsize=20)
-	plt.title (f"Andamento nuovi casi giornalieri {select}", fontsize=20)
-	plt.xlabel ("Giorni", fontsize=20)
-	plt.ylabel ("Nuovi casi", fontsize=20)
-	st.pyplot ()
+
+	figure=px.line (df_reg[df_reg["denominazione_regione"]==select], x="data", y="nuovi_positivi", title=f"Andamento nuovi casi giornalieri {select}",
+				labels={
+				"data": "Data",
+				"nuovi_positivi": "Nuovi casi"
+				})
+	st.plotly_chart (figure, use_container_width=True)
 
 st.header ("Coefficiente di crescita")
 
 if select=="Italia":
-	sns.lineplot (x=df_naz[1:].index, y="crescita", marker="o", data=df_naz[1:])
-	plt.xticks (range(0, len(df_naz.index), 5))
-	plt.yticks (fontsize=20)
-	plt.title ("Andamento nazionale coefficiente di crescita", fontsize=20)
-	plt.xlabel ("Giorni", fontsize=20)
-	plt.ylabel ("Coefficiente di crescita", fontsize=20)
-	st.pyplot ()
+
+	figure=px.line (df_naz, x="data", y="crescita", title="Andamento nazionale coefficiente di crescita",
+				labels={
+				"data": "Data",
+				"crescita": "Coefficiente di crescita"
+				})
+	st.plotly_chart (figure, use_container_width=True)
+
 else:
-	sns.lineplot (x=df_naz[1:].index, y="crescita", marker="o", data=df_reg[df_reg["denominazione_regione"]==select][1:])
-	plt.xticks (range(0, len(df_naz.index), 5))
-	plt.yticks (fontsize=20)
-	plt.title (f"Andamento coefficiente di crescita {select}", fontsize=20)
-	plt.xlabel ("Giorni", fontsize=20)
-	plt.ylabel ("Coefficiente di crescita", fontsize=20)
-	st.pyplot ()
+
+	figure=px.line (df_reg[df_reg["denominazione_regione"]==select], x="data", y="crescita", title=f"Andamento coefficiente di crescita {select}",
+				labels={
+				"data": "Data",
+				"crescita": "Coefficiente di crescita"
+				})
+	st.plotly_chart (figure, use_container_width=True)
 
 st.header ("Casi positivi")
 
 if select=="Italia":
-	sns.lineplot (x=df_naz.index, y="totale_positivi", marker="o", data=df_naz)
-	plt.xticks (range(0, len(df_naz.index), 5))
-	plt.yticks (fontsize=20)
-	plt.title ("Andamento nazionale casi positivi", fontsize=20)
-	plt.xlabel ("Giorni", fontsize=20)
-	plt.ylabel ("Casi positivi", fontsize=20)
-	st.pyplot ()
+
+	figure=px.line (df_naz, x="data", y="totale_positivi", title="Andamento nazionale casi positivi",
+				labels={
+				"data": "Data",
+				"totale_positivi": "Casi positivi"
+				})
+	st.plotly_chart (figure, use_container_width=True)
+
 else:
-	sns.lineplot (x=df_naz.index, y="totale_positivi", marker="o", data=df_reg[df_reg["denominazione_regione"]==select])
-	plt.xticks (range(0, len(df_naz.index), 5))
-	plt.yticks (fontsize=20)
-	plt.title (f"Andamento casi positivi {select}", fontsize=20)
-	plt.xlabel ("Giorni", fontsize=20)
-	plt.ylabel ("Casi positivi", fontsize=20)
-	st.pyplot ()
+
+	figure=px.line (df_reg[df_reg["denominazione_regione"]==select], x="data", y="totale_positivi", title=f"Andamento casi positivi {select}",
+				labels={
+				"data": "Data",
+				"totale_positivi": "Casi positivi"
+				})
+	st.plotly_chart (figure, use_container_width=True)
 
 st.info ("Nota: i *casi positivi* sono calcolati sottraendo deceduti e guariti dai casi totali")
 
 st.header ("Decessi")
 
 if select=="Italia":
-	sns.lineplot (x=df_naz.index, y="deceduti", marker="o", data=df_naz)
-	plt.xticks (range(0, len(df_naz.index), 5))
-	plt.yticks (fontsize=20)
-	plt.title ("Andamento nazionale decessi", fontsize=20)
-	plt.xlabel ("Giorni", fontsize=20)
-	plt.ylabel ("Decessi", fontsize=20)
-	st.pyplot ()
+
+	figure=px.line (df_naz, x="data", y="deceduti", title="Andamento nazionale decessi",
+				labels={
+				"data": "Data",
+				"deceduti": "Decessi"
+				})
+	st.plotly_chart (figure, use_container_width=True)
+
 else:
-	sns.lineplot (x=df_naz.index, y="deceduti", marker="o", data=df_reg[df_reg["denominazione_regione"]==select])
-	plt.xticks (range(0, len(df_naz.index), 5))
-	plt.yticks (fontsize=20)
-	plt.title (f"Andamento decessi {select}", fontsize=20)
-	plt.xlabel ("Giorni", fontsize=20)
-	plt.ylabel ("Decessi", fontsize=20)
-	st.pyplot ()
+
+	figure=px.line (df_reg[df_reg["denominazione_regione"]==select], x="data", y="deceduti", title=f"Andamento decessi {select}",
+				labels={
+				"data": "Data",
+				"deceduti": "Decessi"
+				})
+	st.plotly_chart (figure, use_container_width=True)
 
 st.header ("Guarigioni")
 
 if select=="Italia":
-	sns.lineplot (x=df_naz.index, y="dimessi_guariti", marker="o", data=df_naz)
-	plt.xticks (range(0, len(df_naz.index), 5))
-	plt.yticks (fontsize=20)
-	plt.title ("Andamento nazionale guarigioni", fontsize=20)
-	plt.xlabel ("Giorni", fontsize=20)
-	plt.ylabel ("Guariti", fontsize=20)
-	st.pyplot ()
+
+	figure=px.line (df_naz, x="data", y="dimessi_guariti", title="Andamento nazionale guarigioni",
+				labels={
+				"data": "Data",
+				"dimessi_guariti": "Guariti"
+				})
+	st.plotly_chart (figure, use_container_width=True)
+
 else:
-	sns.lineplot (x=df_naz.index, y="dimessi_guariti", marker="o", data=df_reg[df_reg["denominazione_regione"]==select])
-	plt.xticks (range(0, len(df_naz.index), 5))
-	plt.yticks (fontsize=20)
-	plt.title (f"Andamento guarigioni {select}", fontsize=20)
-	plt.xlabel ("Giorni", fontsize=20)
-	plt.ylabel ("Guariti", fontsize=20)
-	st.pyplot ()
+
+	figure=px.line (df_reg[df_reg["denominazione_regione"]==select], x="data", y="dimessi_guariti", title=f"Andamento guarigioni {select}",
+				labels={
+				"data": "Data",
+				"dimessi_guariti": "Guariti"
+				})
+	st.plotly_chart (figure, use_container_width=True)
 
 st.header ("Suddivisione casi positivi")
 
 if select=="Italia":
+
 	isolamento=df_naz.iloc[-1]["isolamento_domiciliare"]
 	ricoverati=df_naz.iloc[-1]["ricoverati_con_sintomi"]
 	terapia_intensiva=df_naz.iloc[-1]["terapia_intensiva"]
 	x=["Isolamento domiciliare", "Ricoverati generici", "Terapia intensiva"]
 	y=[isolamento, ricoverati, terapia_intensiva]
-	bp=sns.barplot(x=x, y=y)
-	plt.xticks (fontsize=20)
-	plt.yticks (fontsize=20)
-	plt.title ("Suddivisione nazionale casi positivi", fontsize=20)
-	annotate_barplot (bp)
-	st.pyplot ()
+	df_bar=pd.DataFrame ({"x":x, "y":y})
+
+	figure=px.bar (df_bar, x="x", y="y", labels={"x": "Tipologia", "y": "Numerosità"}, text="y")
+	st.plotly_chart (figure, use_container_width=True)
+
 else:
+
 	isolamento=df_reg[df_reg["denominazione_regione"]==select].iloc[-1]["isolamento_domiciliare"]
 	ricoverati=df_reg[df_reg["denominazione_regione"]==select].iloc[-1]["ricoverati_con_sintomi"]
 	terapia_intensiva=df_reg[df_reg["denominazione_regione"]==select].iloc[-1]["terapia_intensiva"]
 	x=["Isolamento domiciliare", "Ricoverati generici", "Terapia intensiva"]
 	y=[isolamento, ricoverati, terapia_intensiva]
-	bp=sns.barplot(x=x, y=y)
-	plt.xticks (fontsize=20)
-	plt.yticks (fontsize=20)
-	plt.title (f"Suddivisione casi positivi {select}", fontsize=20)
-	annotate_barplot (bp)
-	st.pyplot ()
+	df_bar=pd.DataFrame ({"x":x, "y":y})
+
+	figure=px.bar (df_bar, x="x", y="y", labels={"x": "Tipologia", "y": "Numerosità"}, text="y")
+	st.plotly_chart (figure, use_container_width=True)
 
 st.header ("Fonti")
 
